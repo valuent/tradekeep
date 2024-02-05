485efbf378ce1d06c2b3f6d49ebd055ac9207a0f
Anwar Khan <fragsteryt@gmail.com>
2024-02-06 01:45:09 +0530

    edited addtrade

--- src/components/addtradecomp/AddTrade.jsx
+++ src/components/addtradecomp/AddTrade.jsx
@@ -10,6 +10,7 @@ import {
 } from "firebase/firestore";
 import "./addtrade.css";
 import { DataContext } from "../../service/DataContext";
+import addInfosvg from "../../assets/addIcon.svg";
 
 function AddTrade() {
   const { userData, userAuthState, siteDate, userTradeData } =
@@ -18,10 +19,14 @@ function AddTrade() {
   return (
     <div className="addStrategy">
       <div className="title">Add Trade</div>
+
+      {/* Date Selection */}
       <div className="dateSelection">
         <label htmlFor="date">Select date:</label>
         <input type="date" name="dateDropDown" id="dateDropDown" />
       </div>
+
+      {/* StrategySelection */}
       <div className="strategySelection">
         <select name="strategyName" id="">
           <option disabled selected value>
@@ -30,7 +35,11 @@ function AddTrade() {
           <option value="Straddle">Straddle</option>
           <option value="Strangle">Strangle</option>
         </select>
+
+        <label htmlFor="">Enter PnL for "Strategy"</label>
+        <input type="number" name="" id="" step={0.05} />
       </div>
+      <div className="addMoreInfoBtn"></div>
     </div>
   );
 }
