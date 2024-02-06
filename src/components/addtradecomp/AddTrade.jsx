7cfb3709f6da2c28122d196bd6b4398d9c9a4b3e
Anwar Khan <fragsteryt@gmail.com>
2024-02-06 23:56:57 +0530

    updated partial of addtrade

--- src/components/addtradecomp/AddTrade.jsx
+++ src/components/addtradecomp/AddTrade.jsx
@@ -13,33 +13,68 @@ import { DataContext } from "../../service/DataContext";
 import addInfosvg from "../../assets/addIcon.svg";
 
 function AddTrade() {
-  const { userData, userAuthState, siteDate, userTradeData } =
+  const { userData, userAuthState, siteDate, allData } =
     useContext(DataContext);
 
+  const [tradeDataObject, setTradeDataObject] = useState({});
+  const [tradeEntryCount, setTradeEntryCount] = useState();
+
+  useEffect(() => {
+    const countEntries = () => {
+      let dataCount = Object.keys(allData).length;
+      let dataEntryCount = 0;
+      for (let i = 1; i <= dataCount; i++) {
+        let docname = "tradeDoc_" + i;
+        dataEntryCount = dataEntryCount + Object.keys(allData[docname]).length;
+      }
+      setTradeEntryCount(dataEntryCount);
+      console.log("data", dataEntryCount);
+    };
+    countEntries();
+  }, [allData]);
+
+  const setStrategy = () => {};
   return (
-    <div className="addStrategy">
+    <div className="addTradeContainer">
       <div className="title">Add Trade</div>
 
       {/* Date Selection */}
       <div className="dateSelection">
-        <label htmlFor="date">Select date:</label>
+        <label htmlFor="date">Select date</label>
         <input type="date" name="dateDropDown" id="dateDropDown" />
       </div>
 
       {/* StrategySelection */}
-      <div className="strategySelection">
-        <select name="strategyName" id="">
-          <option disabled selected value>
-            --Select Strategy--
-          </option>
-          <option value="Straddle">Straddle</option>
-          <option value="Strangle">Strangle</option>
-        </select>
 
-        <label htmlFor="">Enter PnL for "Strategy"</label>
-        <input type="number" name="" id="" step={0.05} />
+      <div className="strategySelection">
+        <div className="stratname">
+          <label htmlFor="">Select strategy to enter trade data</label>
+          <select name="strategyName" id="">
+            <option disabled selected>
+              --Select Strategy--
+            </option>
+            <option value="Straddle">Straddle</option>
+            <option value="Strangle">Strangle</option>
+          </select>
+        </div>
+
+        <div className="pnl">
+          <label htmlFor="">Enter PnL for "Strategy"</label>
+          <input
+            type="number"
+            name=""
+            id=""
+            step={0.05}
+            placeholder="Profit/Loss"
+          />
+        </div>
       </div>
-      <div className="addMoreInfoBtn"></div>
+      <div className="addMoreInfoBtn">
+        <div className="infoAddingButton">
+          <img src={addInfosvg} />
+          <span>Add Trade-wise Data</span>
+        </div>
+      </div>
     </div>
   );
 }
