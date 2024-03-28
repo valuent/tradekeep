d7b0c0b3b892e75aba0ca981489f982e2251684c
Anwar Khan <fragsteryt@gmail.com>
2024-03-28 23:17:23 +0530

    Deleted node modules

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
+    server: {
+      port: 3006,
+    },
     define: {
       "process.env.SOME_KEY": JSON.stringify(env.SOME_KEY),
     },
